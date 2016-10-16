7d5135d024f48bdffdc3b0d518b57a9128f7e9f1
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-16 00:21:58 -0500

    Add C tournament module to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import numpy.distutils.misc_util
 from setuptools import setup, Extension, find_packages
 
+numpy_includes = numpy.distutils.misc_util.get_numpy_include_dirs()
 pypacwar = Extension(
     "evolpac.duel._PyPacwar",
     ["evolpac/duel/PyPacwar.c", "evolpac/duel/PacWarGuts.c"],
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs()
+    include_dirs=numpy_includes
+)
+tournament = Extension(
+    "evolpac.duel.tournament",
+    ["evolpac/duel/tournament.c", "evolpac/duel/PacWarGuts.c"],
+    include_dirs=numpy_includes,
 )
 
 setup(
@@ -13,7 +19,7 @@ setup(
     description='Evolutionary optimization of PAC-mites',
     author='Shuo Zhao',
     author_email='zhaoshuosve@gmail.com',
-    ext_modules=[pypacwar],
+    ext_modules=[pypacwar, tournament],
     packages=find_packages(),
     install_requires=['numpy']
 )
