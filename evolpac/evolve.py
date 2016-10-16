66bdda80b71e1ccf977d791665786d39e166c282
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-16 17:13:01 -0500

    Update core evolution function for numpy
    Now the population is stored as a mutable numpy array for better
    performance and better interoperability with the C functions.

--- evolpac/evolve.py
+++ evolpac/evolve.py
@@ -2,30 +2,35 @@
 Main driver function for a simple genetic algorithm.
 """
 
+import itertools
 import json
-import operator
 import random
 
-from .genemanip import gen_random_gene, cross, mutate
+import numpy as np
+
+from .genemanip import (
+    gen_random_gene, cross, mutate, GENE_LENGTH, GENE_DT
+)
 
 
 def evolve(
         pop_size, total_steps, score_cb, init=None,
-        gene_length=50, gene_bits=(0, 1, 2, 3),
-        select_ratio=0.4, new_ratio=0.1, breed_new_ratio=0.05,
-        mutate_prob=0.001, n_cross_pts=1, n_mutate_pts=1,
+        select_ratio=0.4, new_ratio=0.1,
+        breed_new_ratio=0.05, n_cross_pts=1,
+        mutate_prob=0.001, n_mutate_pts=1,
         out_prefix='evolution', out_num=20, out_steps=1000, eval_cb=None
 ):
     """Perform the evolution optimization.
 
-    In this simple genetic algorithm implementation, we start with a random
-    population of the given size, to which some given elements can be added.
-    Then the score call-back function is going to be called with the list of
-    all such genes to get the list of pairs of score and the original genes.
-    Then the ``select_ratio`` gives the ratio to be selected to breed.  The
-    resulted evolved population is also able to have some newly generated new
-    genes, controlled by ``new_ratio``. Mutation are only carried out to the
-    new children.
+    In this simple genetic algorithm implementation, a group of Pac-mite genes
+    are going to be stored in an Nx50 numpy byte array.  We start with a random
+    population of the given size, to which some initial elements can be added.
+    Then the score call-back function is going to be called with this array of
+    genes to get a sequence of score for each of them.  Then the
+    ``select_ratio`` gives the ratio to be selected to breed.  The resulted
+    evolved population is also able to have some newly generated new genes,
+    controlled by ``new_ratio``.  Mutation are only carried out to the new
+    children.
 
     A given number of intermediate results can also be written to JSON files
     every any given number of steps.
@@ -33,69 +38,77 @@ def evolve(
     """
 
     # Population initialization.
-    pop = []
+    pop = np.empty((pop_size, GENE_LENGTH), dtype=GENE_DT)
     if init is not None:
-        pop.extend(init)
-    pop.extend(
-        gen_random_gene(gene_length, selection=gene_bits)
-        for _ in range(pop_size - len(pop))
-    )
+        init = []
+    for i, j in itertools.zip_longest(range(pop_size), init):
+        pop[i] = j if j is not None else gen_random_gene()
 
     # Convert the ratios to integral numbers.
     select_num = int(pop_size * select_ratio)
     new_num = int(pop_size * new_ratio)
-    breed_new_num = int(pop_size * breed_new_ratio)
     desc_pairs_num, rem = divmod(pop_size - select_num - new_num, 2)
     select_num += rem
+    breed_new_num = int(new_num * breed_new_ratio)
 
     # Evolution main loop.
     for step_idx in range(total_steps):
 
         # Compute scores.
-        pop_w_score = score_cb(pop)
-        pop_w_score.sort(
-            key=operator.itemgetter(0), reverse=True
-        )  # Skip genes for performance.
+        scores = score_cb(pop)
+        gene_idxes = list(range(pop_size))
+        gene_idxes.sort(key=lambda x: scores[x])
 
-        # Select and output.
-        pop = [i[1] for i in pop_w_score[0:select_num]]
+        # Output.
         if step_idx % out_steps == 0 or step_idx == total_steps - 1:
+            out_idxes = np.array(gene_idxes[-out_num:])
             _dump_pop(
-                pop[0:out_num], '-'.join([out_prefix, str(step_idx)]),
+                pop[out_idxes], scores[out_idxes],
+                '-'.join([out_prefix, str(step_idx)]),
                 eval_cb=eval_cb
             )
 
+        # Gene_idxes will be used as a stack for filling the population.
+        parents = list(gene_idxes[-select_num:])
+        del gene_idxes[-select_num:]
+
         # Add the new blood.
-        pop.extend(
-            gen_random_gene(gene_length, selection=gene_bits)
-            for _ in range(new_num)
-        )
+        for i in range(new_num):
+            idx = gene_idxes.pop()
+            pop[idx] = gen_random_gene()
+            if i < breed_new_num:
+                parents.append(idx)
 
         # Breed.
-        parents = pop[0:select_num + breed_new_num]
         for _ in range(desc_pairs_num):
             parent1, parent2 = random.sample(parents, 2)
-            descs = cross(parent1, parent2, n_pts=n_cross_pts)
+            descs = cross(pop[parent1], pop[parent2], n_pts=n_cross_pts)
             for i in descs:
                 if random.random() < mutate_prob:
-                    mutate(i, n_pts=n_mutate_pts, selection=gene_bits)
-            pop.extend(descs)
+                    mutate(i, n_pts=n_mutate_pts)
+            pop[gene_idxes.pop()] = descs[0]
+            pop[gene_idxes.pop()] = descs[1]
 
+        assert len(gene_idxes) == 0
         continue
 
     return None
 
 
-def _dump_pop(pop, file_name, eval_cb=None):
+def _dump_pop(pop, scores, file_name, eval_cb=None):
     """Dump the population into the given file."""
+
     if eval_cb is None:
-        def eval_cb(genes):
-            return ((None, i) for i in genes)
+        evals = [None for _ in pop]
+    else:
+        evals = eval_cb(pop)
+
+    res = [
+        {'gene': i, 'score': j, 'eval': k}
+        for i, j, k in zip(pop, scores, evals)
+        ]
 
     with open(file_name + '.json', 'w') as out_fp:
-        json.dump(
-            [{'gene': gene, 'eval': eval} for eval, gene in eval_cb(pop)],
-            out_fp
-        )
+        json.dump(res, out_fp)
 
     return None
