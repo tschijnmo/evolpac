4ff465279a169212783982416f8f1d63b99752e7
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-16 03:13:13 -0500

    Remove python average score function
    It duplicates its functionality with the tournament functions.

--- evolpac/avescore.py
+++ evolpac/avescore.py
@@ -5,23 +5,6 @@ import itertools
 from .duel import duel
 
 
-def compute_cross_ave_score(genes):
-    """Computes the cross average score of all duels among the genes."""
-
-    n_genes = len(genes)
-    scores = [0 for _ in genes]
-    for i, j in itertools.combinations(range(n_genes), 2):
-        s_i, s_j = duel(genes[i], genes[j])
-        scores[i] += s_i
-        scores[j] += s_j
-        continue
-
-    factor = 1.0 / (n_genes - 1)
-    return [
-        (i * factor, j) for i, j in zip(scores, genes)
-        ]
-
-
 def compute_ave_score_w_sample(genes, samples):
     """Computes the average score for duelling with a random sample."""
 
