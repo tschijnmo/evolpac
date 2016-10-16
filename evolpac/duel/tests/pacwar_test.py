8982fe9ad7ae60b063f459626e5f054cd8067cb5
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-16 01:16:35 -0500

    Add test for the C tournament function

--- evolpac/duel/tests/pacwar_test.py
+++ evolpac/duel/tests/pacwar_test.py
@@ -2,7 +2,10 @@
 
 import unittest
 
-from evolpac.duel._PyPacwar import battle
+import numpy as np
+
+from evolpac.duel.duel import battle
+from evolpac.duel import run_duel, run_tournament
 
 class CoreTest(unittest.TestCase):
     """Tests for the core C extension."""
@@ -14,3 +17,15 @@ class CoreTest(unittest.TestCase):
         self.assertEqual(rounds, 500)
         self.assertEqual(c1, c2)
         self.assertEqual(c1, 57)
+
+    def test_duel_tour(self):
+        """Test duel and tournament has same result."""
+        genes_duel = [[1] * 50, [3] * 50]
+        genes_tour = np.array(genes_duel, dtype=np.byte)
+
+        duel_res = run_duel(*genes_duel)
+        tour_res = run_tournament(genes_tour)
+
+        for i in range(0, 2):
+            self.assertEqual(duel_res[i], tour_res[i])
+
